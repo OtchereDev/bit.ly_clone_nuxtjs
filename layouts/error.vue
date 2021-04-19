4008ed46a5e2e7d4557d3de5a776a084ee9f9044
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-19 11:00:23 +0000

    added a custom error layout

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="w-full pt-20 bg-gray-200 min-h-screen" >
+      <h2 class='text-center text-4xl text-gray-800 mb-10 font-semibold'>
+        Seems You are Lost Or Your Link is Wrong 😖
+      </h2>
+      <div class="w-4/12 mx-auto flex justify-center items-center">
+
+        <img :src="require('assets/undraw_page_not_found_su7k.svg')" alt="404 image" class="w-full">
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
